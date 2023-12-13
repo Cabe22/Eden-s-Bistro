cc3ea96ec8f8bca298266725ae0fde4e3e0fea11
Cabe22 <125468641+Cabe22@users.noreply.github.com>
2023-12-12 16:58:27 -0800

    Updated Events page
    Made it look more professional

--- Events.php
+++ Events.php
@@ -11,7 +11,7 @@
 <body>
     <nav class="navbar"> <!--Navigation bar-->
         <div class="navbar__container"> 
-            <img src="Eden's Bistro logo.png" alt="Eden's Bistro logo" id="navbarImage">
+            <img src="Eden's Bistro logo two.png" alt="Eden's Bistro logo" id="navbarImage">
             <div id="navbarTitle">Eden's Bistro</div>
         <ul class="navbar__1"> 
             <li class="navbar__item"> 
